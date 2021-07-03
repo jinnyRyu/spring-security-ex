570c5149ce23eb07a26419a8496771703e0afb26
jinnyRyu <hajinryu51@gmail.com>
2021-07-03 18:39:52 +0900

    jpa repository method규칙, nativesql 사용하기

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,9 +1,22 @@
 package org.zerock.ex2.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.ex2.entity.Memo;
 
 public interface MemoRepository extends JpaRepository<Memo,Long> {
     
+    //mno 사이값을 이용하여 역순으로 범위지정해서 값 구하기 
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
     
+    Page<Memo> findByMnoBetween(Long from, Long to,  Pageable pageable);
+
+    // void deleteMemoByMnoLessThan(Long num);
+
+    @Query(value = "select * from tbl_memo where mno>0 " , nativeQuery = true)
+    List<Memo> getNativeResult();
 }
--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -1,10 +1,16 @@
 package org.zerock.ex2.repository;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+
 import org.zerock.ex2.entity.Memo;
 
 @SpringBootTest
@@ -17,15 +23,60 @@ public class MemoRepositoryTests {
     //     System.out.println(memoRepository.getClass().getName());
     // }
 
-    @Test
-    public void testInsert(){
-     IntStream.rangeClosed(1,100).forEach(i->{
-        // Memo memo = Memo.builder().memoText("sample.."+i).build();
-        Memo memo = Memo.builder().memoText("sample.."+i).build();
-        memoRepository.save(memo);
+    // @Test
+    // public void testInsert(){
+    //  IntStream.rangeClosed(1,100).forEach(i->{
+    //     // Memo memo = Memo.builder().memoText("sample.."+i).build();
+    //     Memo memo = Memo.builder().memoText("sample.."+i).build();
+    //     memoRepository.save(memo);
 
-     });
+    //  });
+    // }
+
+
+    // @Test
+    // public void testPageDefault(){
+    //     Pageable pageable = PageRequest.of(0, 10);
+    //     Page<Memo> result = memoRepository.findAll(pageable);
+    //     System.out.println(result);
+
+
+       
+    
+    // }
+
+    // @ Test
+    // public void testQueryMethodes(){
+
+    //     List <Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+    //     for(Memo memo : list){
+    //         System.out.println(memo);
+    //     }
+    // }
+
+    // @Test
+    // public void testQueryMethodeWithPageable(){
+    //     Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+    //     Page<Memo> result = memoRepository.findByMnoBetween(10L, 20L, pageable);
+
+    //     result.get().forEach(memo->System.out.println(memo));
+
+
+    // }
+
+    @Test
+    public void testNativeQueryList(){
+
+      
+        List<Memo> result = memoRepository.getNativeResult();
+        
+        for(Memo memo : result){
+            System.out.println(memo);
+        }
+
     }
+
 
 
     
